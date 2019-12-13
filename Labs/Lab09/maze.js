d3ef04efa3040dbb64eacada79a6f7a1400f5073
dmahgoub <dmahgoub@mum.edu>
2019-12-13 13:40:18 -0600

    Maze modified

---
+++ Labs/Lab09/maze.js
@@ -0,0 +1,29 @@
+(function () {
+    let reset = function () {
+        $(this).data('clicked', true);
+        $(".boundary").removeClass('youlose');
+        $("#status").html('Click the "S" to begin.');
+    }
+
+    let red = function () {
+        if ($("#start").data('clicked')) {
+            $(".boundary").addClass('youlose');
+            $("#status").html('Sorry You Lost! =(');
+        }
+    }
+
+    let end = function () {
+        if ($("#start").data('clicked')) {
+            if (!($(".boundary").hasClass('youlose')))               
+                $("#status").html('You Win! =)');
+            $("#start").data('clicked', false);
+        }
+    }
+
+    $(document).ready(
+        $("#start").click(reset),
+        $(".boundary").mouseover(red),
+        $("#maze").mouseleave(red),
+        $("#end").mouseover(end)
+    );
+})();
