c233fde82ddb65cf16936de424bff2ede1a22cd4
dmahgoub <dmahgoub@mum.edu>
2019-12-16 14:40:39 -0600

    15 Puzzle Modified

--- Homeworks/HW06/fifteen.js
+++ Homeworks/HW06/fifteen.js
@@ -1,26 +1,76 @@
-let main = (function () {
+let addjQuery = (function () {
+    // Adding jQuery
+    let jquery = document.createElement('script');
+    jquery.src = 'https://code.jquery.com/jquery-3.4.1.min.js';
+    jquery.type = 'text/javascript';
+    let head = document.getElementsByTagName('head');
+    head[0].appendChild(jquery);
+})();
+window.onload = function () {
 
-    let interval;
-    
-    let importScript = function () {
-        // jQuery
-        let jquery = document.createElement('script');
-        jquery.src = 'https://code.jquery.com/jquery-3.4.1.min.js';
-        jquery.type = 'text/javascript';
-        let head = document.getElementsByTagName('head');
-        head[0].appendChild(jquery);
-        // Script
-        let script = document.createElement('script');
-        script.src = 'script.js';
-        script.type = 'text/javascript';
-        let body = document.getElementsByTagName('body');
-        body[body.length - 1].appendChild(script);
-        clearInterval(interval);
+    let coordinates = [];
+
+    // Initializing puzzle
+    let init = function () {
+        $("#puzzlearea div").each(function (i) {
+            // calculate x and y for this piece
+            let x = ((i % 4) * 100);
+            let y = (Math.floor(i / 4) * 100);
+            // set basic style and background
+            $(this).addClass('puzzlepiece');
+            $(this).css('left', x + 'px');
+            $(this).css('top', y + 'px');
+            $(this).css('background-image', 'url("background.jpg")');
+            $(this).css('background-position', -x + 'px ' + (-y) + 'px');
+            // store x and y for later
+            // $(this).x = x;
+            // $(this).y = y;
+            $(this).coordinate = { x: x, y: y }
+            coordinates.push($(this).coordinate);
+        });
     };
 
-    interval = setInterval(importScript, 500);
-    
+    init();
 
-})();
+    function shuffle() {
+        // $("#puzzlearea div").each(function (i) {
+        //     // calculate x and y for this piece
+
+        //     let x = ((i % 4) * 100);
+        //     let y = (Math.floor(i / 4) * 100);
+        //     // set basic style and background
+        //     $(this).addClass('puzzlepiece');
+        //     $(this).css('left', x + 'px');
+        //     $(this).css('top', y + 'px');
+        //     $(this).css('background-image', 'url("background.jpg")');
+        //     $(this).css('background-position', -x + 'px ' + (-y) + 'px');
+        //     // store x and y for later
+        //     $(this).x = x;
+        //     $(this).y = y;
+        //     let coordinate = { x: x, y: y }
+        //     coordinates.push(coordinate);
+        // });
+    }
+
+
+    // Event Handlers
+    $("#shufflebutton").click(function () {
+        shuffle();
+    });
+
+    $("#puzzlearea div").hover(function () {
+        // if moveable 
+        if (true) {
+            $(this).addClass('movablepiece');
+        }
+    });
+
+    $("#puzzlearea div").click(function () {
+        // if moveable 
+        if (true) {
+            swap();
+        }
+    });
+};
 
 
